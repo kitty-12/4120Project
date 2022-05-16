199d0e9de53f251b0d18f382956547fbc5c3fea1
LIU Shuyi <1155124549@link.cuhk.edu.hk>
2022-05-16 21:40:36 +0800

    add dialogue dispaly script

--- Adventure in Fairy Tale/Assets/DialogScripts/Dialog.cs
+++ Adventure in Fairy Tale/Assets/DialogScripts/Dialog.cs
@@ -8,17 +8,25 @@ public class Dialog : MonoBehaviour
     public TextMeshProUGUI textDisplay;
     public string[] sentences;
     public float typeSpeed = 0.02f;
+    public GameObject continueButton;
+    public GameObject closeButton;
+    public GameObject panel;
     private int id;
+
+    private AudioSource source;
     // Start is called before the first frame update
     void Start()
     {
+        source = GetComponent<AudioSource>();
         StartCoroutine(Type());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(textDisplay.text == sentences[id]){
+            continueButton.SetActive(true);
+        }
     }
 
     IEnumerator Type(){
@@ -26,5 +34,23 @@ public class Dialog : MonoBehaviour
             textDisplay.text += letter;
             yield return new WaitForSeconds(typeSpeed);
         }
+    }
+
+    public void NextSentence(){
+        source.Play();
+        continueButton.SetActive(false);
+        if(id < sentences.Length - 1) {
+            id++;
+            textDisplay.text = "";
+            StartCoroutine(Type());
+        } else {
+            textDisplay.text = "";
+            panel.SetActive(false);
+            continueButton.SetActive(false);
+        }
+    }
+
+    public void ClosePanel(){
+        panel.SetActive(false);
     }
 }
