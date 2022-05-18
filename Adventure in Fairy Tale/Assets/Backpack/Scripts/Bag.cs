4b20b0954454eb51f62c1a7a0c2f7cde48f8b9ca
YF-0111 <1155124411@link.cuhk.edu.hk>
2022-05-18 10:46:25 +0800

    b

---
+++ Adventure in Fairy Tale/Assets/Backpack/Scripts/Bag.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bag : MonoBehaviour
+{
+    // Rigidbody2D rb;
+    // Collider2D coll;
+    // Animator anim;
+    public GameObject myBag;
+    bool isBagOpen;
+  
+
+    private void Awake()
+    {
+        // rb = GetComponent<Rigidbody2D>();
+        // coll = GetComponent<Collider2D>();
+        // anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        OpenMyBag();
+    }
+
+    void OpenMyBag()
+    {
+        if(Input.GetKeyDown(KeyCode.O)){
+            isBagOpen = !isBagOpen;
+            myBag.SetActive(isBagOpen);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # 4120Project
 A repository for 4120 computer game project.
 111
+
+MyBag -> Backpack System
+Bag.cs -> click o to control the backpack system to appear or close
+Item.cs -> store info of items
+Inventory.cs -> this is a list
+ItemOnWorld.cs -> pick up items and store them
