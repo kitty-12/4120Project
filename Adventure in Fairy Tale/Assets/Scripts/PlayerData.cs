efc4c6ecbbb0879ac6f6abfc368dd4d0cf7ec5f4
Eri-Xuan <62581606+Eri-Xuan@users.noreply.github.com>
2022-05-18 17:41:54 +0800

    add fire spell

--- Adventure in Fairy Tale/Assets/Scripts/PlayerData.cs
+++ Adventure in Fairy Tale/Assets/Scripts/PlayerData.cs
@@ -17,7 +17,12 @@ public class PlayerData : MonoBehaviour
     [Header("Others")]
     public Animator anim;
 
+    public GameObject FireSpellPrefab;
+
+    public GameObject selectedEnemy;
+
     private bool NormalAttacking = false;
+    private bool selected = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -29,7 +34,20 @@ public class PlayerData : MonoBehaviour
 
     void Update() 
     {
+        if (selected){
+            Vector3 targetPos = new Vector3(selectedEnemy.transform.position.x,selectedEnemy.transform.position.y,selectedEnemy.transform.position.z);
+            float dis = Vector3.Distance(selectedEnemy.transform.position,this.transform.position);
+            if (dis>= 30.0f){
+                selectedEnemy = null;
+                selected = false;
+            }
+        }
         if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            //StartCoroutine(NormalAtk());
+            SelectTarget();
+        }
+        if (Input.GetKeyDown(KeyCode.Mouse1))
         {
             StartCoroutine(NormalAtk());
         }
@@ -60,14 +78,36 @@ public class PlayerData : MonoBehaviour
     IEnumerator NormalAtk(){
         NormalAttacking = true;
         anim.SetBool("NormalAtk",true);
-        yield return new WaitForSeconds(1.5f);
+        yield return new WaitForSeconds(1.0f);
+        FireSpell();
         anim.SetBool("NormalAtk",false);
         NormalAttacking = false;
     }
     IEnumerator Death(){
         anim.SetBool("Death",true);
         yield return new WaitForSeconds(4);
-        Application.LoadLevel(2);
+        Application.LoadLevel(3);
         anim.SetBool("Death",false);
+    }
+
+    void FireSpell(){
+        if (selected){
+            Vector3 SpawnSpellLoc = new Vector3(this.transform.position.x,this.transform.position.y,this.transform.position.z);
+            GameObject fire;
+            fire = Instantiate(FireSpellPrefab,SpawnSpellLoc,Quaternion.identity);
+            fire.transform.GetComponent<Spell>().Target = selectedEnemy;
+        }
+    }
+
+    void SelectTarget(){
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray,out hit,10000)){
+            if(hit.transform.gameObject.CompareTag("Enemy")){
+                //Debug.Log("here");
+                selectedEnemy = hit.transform.gameObject;
+                selected = true;
+            }
+        }
     }
 }
